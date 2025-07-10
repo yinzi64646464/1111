5fb018565df437835b2d9da8df51239f53813400
yinzi64646464 <figure2025@126.com>
2025-07-10 20:27:52 +0800

    Create guinew.lua

---
+++ guinew.lua
@@ -0,0 +1,37 @@
+local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/DevSloPo/Main/refs/heads/main/main.lua"))()
+
+local Window = WindUI:CreateWindow({
+    Title = "问号脚本丨选择服务器",
+    Icon = "moon",
+    Author = "作者:小银子",前面代码是抄的xk 
+    Folder = "CloudHub",
+    Size = UDim2.fromOffset(400, 300),
+    Transparent = true,
+    Theme = "Sky",
+    User = {
+        Enabled = true, 
+        Callback = function() print("clicked") end, 
+        Anonymous = true 
+    },
+})
+
+Window:EditOpenButton({
+    Title = "      开启UI      ",
+    Icon = "eye",
+    CornerRadius = UDim.new(0,16),
+    StrokeThickness = 2,
+    Color = ColorSequence.new( 
+        Color3.fromHex("FF00FF"), 
+        Color3.fromHex("00FFFF"), 
+        Color3.fromHex("800080")   
+    ),
+    Draggable = true,
+})
+local Tabs = {
+    N = Window:Tab({ Title = "服务器", Icon = "house", Desc = "狗屎" }),
+  Tabs.N:Button({
+    Title = "没做",
+    Callback = function()
+    114514
+    end
+})
